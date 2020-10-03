7e8eaee1038997ed24600a596e91028b665db093
MArcon Willian <marconwillian@protonmail.com>
2020-10-03 16:32:27 -0300

    chore: Finish validate erros in register page

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
+
+import Tooltip from "../Tooltip";
 
 interface ContainerProps {
   isFocused: boolean;
   isField: boolean;
+  hasError: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -21,32 +24,56 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
 
-  ${props => props.isFocused &&
+  ${(props) =>
+    props.hasError &&
     css`
-      border-color: #FF9000;
-      color: #FF9000;
-    `
-  }
+      border-color: #c53030;
+    `}
 
-  ${props => props.isField &&
+  ${(props) =>
+    props.isFocused &&
     css`
-      color: #FF9000;
-    `
-  }
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isField &&
+    css`
+      color: #ff9000;
+    `}
+
 
   input {
     flex: 1;
     background: transparent;
     border: 0;
-    color: #F4EDE8;
+    color: #f4ede8;
 
     &::placeholder {
       color: #666360;
     }
-
   }
 
   svg {
     margin-right: 16px;
   }
-`
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
